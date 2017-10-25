19ee39d2f36210d3f3cf4e8c9ab4d7d25dd1ea86
Quentin Rossettini <quentin.rossettini@mail.utoronto.ca>
2017-10-25 08:55:55 +0200

    e

--- test-api-ai.js
+++ test-api-ai.js
@@ -13,7 +13,7 @@ app.all('/', function (req, res){
 	console.log("hello");
 	var response = "Réponse du serveur";
 	res.setHeader('Content-Type', 'application/json');
-	response += req.body.result.parameters."geo-city";
+	//response += req.body.result.parameters."geo-city";
 	res.send(JSON.stringify({
 		speech : response, 
 		displayText : response, 
